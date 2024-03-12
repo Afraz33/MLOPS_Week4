302257ab0a4c2942dc63cb08eb5d5f1fecc4441d
Afraz33 <afraz3301@gmail.com>
2024-03-12 22:36:10 +0500

    Commit 4 to test build

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class mlops:
         return "MLOPS (CS-B)"
 
 
-# random comment 3 to test build
+# random comment 3 to test builds
 mlops_class = mlops(5)
 mlops_class.addStudents()
 mlops_class.removeStudent()
