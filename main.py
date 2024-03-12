96b5807996d8dc125179cce61a087144f2e90592
Afraz33 <afraz3301@gmail.com>
2024-03-12 22:26:48 +0500

    Random Commit to test build

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 class mlops:
-	def __init__(self, totalStudents):
-		self.totalStudents = totalStudents
+    def __init__(self, totalStudents):
+        self.totalStudents = totalStudents
 
-	def getTotalStudents(self):
-		return self.totalStudents
+    def getTotalStudents(self):
+        return self.totalStudents
 
-	def addStudents(self):
-		self.totalStudents = self.totalStudents+1
+    def addStudents(self):
+        self.totalStudents = self.totalStudents+1
 
-	def removeStudent(self):
-		self.totalStudents = self.totalStudents -1
+    def removeStudent(self):
+        self.totalStudents = self.totalStudents - 1
+
+    def getClassName(self):
+        return "MLOPS (CS-B)"
 
 
-	def getClassName(self):
-		return "MLOPS (CS-B)"
-
+# random comment to test build
 mlops_class = mlops(5)
 mlops_class.addStudents()
 mlops_class.removeStudent()
 print(mlops_class.getTotalStudents())
 print(mlops_class.getClassName())
-
-
