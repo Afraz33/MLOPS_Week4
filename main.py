4b8bfbf0d1f4886b9b4f2b932d2c139dd7d00fb8
Afraz33 <afraz3301@gmail.com>
2024-03-22 20:12:25 +0500

    test workflow 3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class mlops:
         return "MLOPS (CS-B)"
 
 
-# random comment 8 to test builds
+# random comment 9 to test builds
 mlops_class = mlops(5)
 mlops_class.addStudents()
 mlops_class.removeStudent()
